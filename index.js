9525c93c567cac3afeea24b249b08ef4667c233a
SoundBrake <mesuper222@gmail.com>
2018-07-11 15:46:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // Write your code in this file!
 
+function getUKTime(){
+  return new Date().getHours() + 5
+}
 
+function discountTicket(x){
+  return x * 0.8
+}
+
+function tweetReply(x){
+  return "Hey "+ x + ", thanks so much for the support! I really appreciate it!"
+}
